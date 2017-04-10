4db891e81e854267b8c28c63dd7cdb8d59d7a864
Benjamin Petetot <bpetetot@gmail.com>
2017-04-10 18:00:24 +0200

    test detect props

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
 #!/usr/bin/env node
 
-const path = require('path')
-const Promise = require('bluebird')
-const fs = Promise.promisifyAll(require('fs'))
-const mkdirp = Promise.promisifyAll(require('mkdirp'))
-const webpackDevServer = require('./webpack')
+const path = require('path');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const mkdirp = Promise.promisifyAll(require('mkdirp'));
+const webpackDevServer = require('./webpack');
 
-const start = async (component) => {
+const start = async component => {
   // 1. Read the template file
-  const filePath = path.resolve(__dirname, 'template.jsx')
-  const template = await fs.readFileAsync(filePath, 'utf-8')
+  const filePath = path.resolve(__dirname, 'template.jsx');
+  const template = await fs.readFileAsync(filePath, 'utf-8');
 
   // 2. Replace what needs to be replaced
-  const componentPath = path.relative(__dirname, path.resolve(process.env.PWD, component))
-  const appFileContent = template.replace('/* react-workbench-insert import */', componentPath)
+  const componentPath = path.relative(__dirname, path.resolve(process.env.PWD, component));
+  const appFileContent = template.replace('/* react-workbench-insert import */', componentPath);
 
   // 3. Write it into a tmp folder
   // 3.a Create the tmp folder
-  const output = { dir: path.resolve(__dirname, '..', 'tmp'), file: 'index.jsx' }
+  const output = { dir: path.resolve(__dirname, '..', 'tmp'), file: 'index.jsx' };
   try {
-    await fs.mkdirAsync(output.dir)
+    await fs.mkdirAsync(output.dir);
   } catch (ex) {
-    if (ex.errno !== -17) { // -17 is directory already exists
-      throw ex
+    if (ex.errno !== -17) {
+      // -17 is directory already exists
+      throw ex;
     }
   }
 
   // 3.b Write file
-  await fs.writeFileAsync(path.resolve(output.dir, output.file), appFileContent)
+  await fs.writeFileAsync(path.resolve(output.dir, output.file), appFileContent);
 
   // 4. Start webpack-dev-server
-  webpackDevServer.start()
-}
+  webpackDevServer.start();
+};
 
-start(process.argv[2])
+start(process.argv[2]);
