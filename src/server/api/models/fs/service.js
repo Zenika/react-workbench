8763de9ed387dc4e9a542263d7dc4f6dec73ae7f
Fabien JUIF <fabien.juif@zenika.com>
2017-04-25 17:55:20 +0200

    :sparkles: browse API (#35)

---
+++ src/server/api/models/fs/service.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+
+const ls = async (entryPath) => {
+  const files = await fs.readdirAsync(entryPath)
+
+  const statsFiles = files.map(async (fullname) => {
+    const stats = await fs.statAsync(path.resolve(entryPath, fullname))
+    const ext = path.extname(fullname)
+    const name = path.basename(fullname, ext)
+
+    return Object.assign(
+      {},
+      {
+        fullname,
+        name,
+        ext,
+        isDirectory: stats.isDirectory(),
+      },
+      stats // eslint-disable-line comma-dangle
+    )
+  })
+
+  return Promise.all(statsFiles)
+}
+
+module.exports = {
+  ls,
+}
