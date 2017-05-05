ca4356579485919fa6bd00edbc613cea69787dfc
Fabien JUIF <fabien.juif@zenika.com>
2017-05-05 17:29:05 +0200

    :white_check_mark: first component test

---
+++ src/gui/components/navbar/navbar.spec.js
@@ -0,0 +1,11 @@
+/* eslint-env jest */
+import snap from 'misc/snap'
+import Navbar from './navbar'
+
+const snapshot = snap(Navbar)
+
+describe('component/navbar', () => {
+  it('should take custom className', snapshot({ className: 'custom' }))
+  it('should take custom style', snapshot({ style: { backgroundColor: 'red' } }))
+  it('should have a default behaviour', snapshot({ }))
+})
