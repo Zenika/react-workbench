47437bfb8af78e778c6e40f8e0da0c30ed424751
Benjamin Petetot <bpetetot@gmail.com>
2017-05-08 20:42:20 +0200

    add error messages when package.json and node_modules not found

--- src/server/webpack/modules.js
+++ src/server/webpack/modules.js
@@ -10,9 +10,28 @@ const get = async (component) => {
   // looking for the first directory with a 'package.json' file
   while (!files.includes('package.json')) {
     previousPath = path.resolve(previousPath, '..')
-    log.debug(`Looking for package.json into '${previousPath}`)
+    log.debug(`Looking for package.json into [${previousPath}]`)
     files = await fs.readdirAsync(previousPath)
+
+    // basename is empty, we are at the file system root
+    if (path.basename(previousPath) === '') {
+      log.error('package.json not found, unable to identify project root')
+      process.exit(1)
+    }
   }
+
+  // check if node_modules exists
+  try {
+    await fs.statAsync(`${previousPath}/node_modules`)
+  } catch (ex) {
+    if (ex.errno === -2) { // -2 is file not found
+      log.error(`node_modules doesn't exist in [${previousPath}], please resolve dependencies`)
+      process.exit(1)
+    } else {
+      throw ex
+    }
+  }
+
   paths.push(`${previousPath}/node_modules`)
   paths.push(previousPath)
 
