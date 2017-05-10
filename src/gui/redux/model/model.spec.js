4c7abb6c980b2529bd517065487a508d921b3040
Benjamin Petetot <bpetetot@gmail.com>
2017-05-10 16:45:25 +0200

    remove test of action creators

--- src/gui/redux/model/model.spec.js
+++ src/gui/redux/model/model.spec.js
@@ -3,128 +3,99 @@ import { SET_MODEL, SET_VALUE, setModel, setValue } from './model.actions'
 import reducer from './model'
 
 describe('gui/redux/model', () => {
-  describe('actions', () => {
-    it(`should generate ${SET_MODEL} action`, () => {
+  it('should initialize state', () => {
+    // execute
+    const state = reducer(undefined, { type: undefined })
+
+    // asserts
+    expect(state).toBeDefined()
+    expect(state).toEqual({ })
+  })
+
+  describe(`${SET_MODEL} action`, () => {
+    it('should update state with model', () => {
       // data
       const model = { foo: {}, bar: {} }
 
       // execute
-      const action = setModel(model)
-
-      // asserts
-      expect(action).toBeDefined()
-      expect(action).toEqual({ type: SET_MODEL, payload: model })
-    })
-
-    it(`should generate ${SET_VALUE} action`, () => {
-      // data
-      const name = 'foo'
-      const value = 'bar'
-
-      // execute
-      const action = setValue(name, value)
-
-      // asserts
-      expect(action).toBeDefined()
-      expect(action).toEqual({ type: SET_VALUE, payload: { name, value } })
-    })
-  })
-
-  describe('reducer', () => {
-    it('should initialize state', () => {
-      // execute
-      const state = reducer(undefined, { type: undefined })
+      const state = reducer(undefined, setModel(model))
 
       // asserts
       expect(state).toBeDefined()
-      expect(state).toEqual({ })
+      expect(state).toEqual(model)
+    })
+  })
+
+  describe(`${SET_VALUE} action`, () => {
+    it('should update state with "boolean" value', () => {
+      // data
+      const prevState = { foo: { type: 'bool' } }
+
+      // execute
+      const state = reducer(prevState, setValue('foo', true))
+
+      // asserts
+      expect(state).toBeDefined()
+      expect(state.foo).toBeDefined()
+      expect(typeof state.foo.value).toBe('boolean')
+      expect(state.foo.value).toBe(true)
     })
 
-    describe(`${SET_MODEL} action`, () => {
-      it('should update state with model', () => {
-        // data
-        const model = { foo: {}, bar: {} }
+    it('should update state with "object" value', () => {
+      // data
+      const prevState = { foo: { type: 'object' } }
 
-        // execute
-        const state = reducer(undefined, setModel(model))
+      // execute
+      const state = reducer(prevState, setValue('foo', '{ bar: \'val\' }'))
 
-        // asserts
-        expect(state).toBeDefined()
-        expect(state).toEqual(model)
-      })
+      // asserts
+      expect(state).toBeDefined()
+      expect(state.foo).toBeDefined()
+      expect(typeof state.foo.value).toBe('object')
+      expect(state.foo.value).toEqual({ bar: 'val' })
     })
 
-    describe(`${SET_VALUE} action`, () => {
-      it('should update state with "boolean" value', () => {
-        // data
-        const prevState = { foo: { type: 'bool' } }
+    it('should update state with "array" value', () => {
+      // data
+      const prevState = { foo: { type: 'array' } }
 
-        // execute
-        const state = reducer(prevState, setValue('foo', true))
+      // execute
+      const state = reducer(prevState, setValue('foo', '[\'item1\', \'item2\']'))
 
-        // asserts
-        expect(state).toBeDefined()
-        expect(state.foo).toBeDefined()
-        expect(typeof state.foo.value).toBe('boolean')
-        expect(state.foo.value).toBe(true)
-      })
+      // asserts
+      expect(state).toBeDefined()
+      expect(state.foo).toBeDefined()
+      expect(typeof state.foo.value).toBe('object')
+      expect(state.foo.value).toEqual(['item1', 'item2'])
+    })
 
-      it('should update state with "object" value', () => {
-        // data
-        const prevState = { foo: { type: 'object' } }
+    it('should update state with "func" value', () => {
+      // data
+      const prevState = { foo: { type: 'func' } }
+      const func = '() => "hello world"'
 
-        // execute
-        const state = reducer(prevState, setValue('foo', '{ bar: \'val\' }'))
+      // execute
+      const state = reducer(prevState, setValue('foo', func))
 
-        // asserts
-        expect(state).toBeDefined()
-        expect(state.foo).toBeDefined()
-        expect(typeof state.foo.value).toBe('object')
-        expect(state.foo.value).toEqual({ bar: 'val' })
-      })
+      // asserts
+      expect(state).toBeDefined()
+      expect(state.foo).toBeDefined()
+      expect(typeof state.foo.value).toBe('function')
+      expect(state.foo.value.toString()).toEqual(func)
+    })
 
-      it('should update state with "array" value', () => {
-        // data
-        const prevState = { foo: { type: 'array' } }
+    it('should update state with "string" value', () => {
+      // data
+      const prevState = { foo: { type: 'string' } }
 
-        // execute
-        const state = reducer(prevState, setValue('foo', '[\'item1\', \'item2\']'))
+      // execute
+      const state = reducer(prevState, setValue('foo', 'bar'))
 
-        // asserts
-        expect(state).toBeDefined()
-        expect(state.foo).toBeDefined()
-        expect(typeof state.foo.value).toBe('object')
-        expect(state.foo.value).toEqual(['item1', 'item2'])
-      })
-
-      it('should update state with "func" value', () => {
-        // data
-        const prevState = { foo: { type: 'func' } }
-        const func = '() => "hello world"'
-
-        // execute
-        const state = reducer(prevState, setValue('foo', func))
-
-        // asserts
-        expect(state).toBeDefined()
-        expect(state.foo).toBeDefined()
-        expect(typeof state.foo.value).toBe('function')
-        expect(state.foo.value.toString()).toEqual(func)
-      })
-
-      it('should update state with "string" value', () => {
-        // data
-        const prevState = { foo: { type: 'string' } }
-
-        // execute
-        const state = reducer(prevState, setValue('foo', 'bar'))
-
-        // asserts
-        expect(state).toBeDefined()
-        expect(state.foo).toBeDefined()
-        expect(typeof state.foo.value).toBe('string')
-        expect(state.foo.value).toEqual('bar')
-      })
+      // asserts
+      expect(state).toBeDefined()
+      expect(state.foo).toBeDefined()
+      expect(typeof state.foo.value).toBe('string')
+      expect(state.foo.value).toEqual('bar')
     })
   })
 })
