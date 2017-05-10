3439450f10d4778acab98991e26a55b048d60d4d
Benjamin Petetot <bpetetot@gmail.com>
2017-05-10 16:33:18 +0200

    :white_check_mark: 'props' component (#70)

--- src/gui/components/props/props.spec.js
+++ src/gui/components/props/props.spec.js
@@ -1,9 +1,35 @@
+/* eslint-disable react/jsx-filename-extension */
 /* eslint-env jest */
-// eslint-disable-next-line
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import snap from 'misc/snap'
 import Props from './props'
-// eslint-disable-next-line
 import PropsContainer from './props.container'
 
-describe('todo', () => {
-  it('todo')
+// mock input component into props
+jest.mock('./input', () => 'input')
+
+describe('component/props', () => {
+  describe('props.jsx', () => {
+    const snapshot = snap(Props)
+    it('should take custom className', snapshot({ className: 'custom' }))
+    it('should take custom style', snapshot({ style: { backgroundColor: 'red' } }))
+    it('should render by default', snapshot({ }))
+    it('should render inputs following given properties', snapshot({ properties: ['prop1', 'prop2'] }))
+  })
+
+  describe('props.container.js', () => {
+    it('should map redux model into properties array', () => {
+      const store = createStore(() => ({ model: { foo: { type: 'string' }, bar: { type: 'string' } } }))
+      const container = renderer.create(
+        <Provider store={store}>
+          <PropsContainer />
+        </Provider>
+      )
+      const tree = container.toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
 })
