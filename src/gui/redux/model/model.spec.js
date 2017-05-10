161525089f3f37148c93b4c57d7f417c0246c9d0
Benjamin Petetot <bpetetot@gmail.com>
2017-05-10 23:19:17 +0200

    :white_check_mark: 'model' reducer & actions  (#71)

---
+++ src/gui/redux/model/model.spec.js
@@ -0,0 +1,101 @@
+/* eslint-env jest */
+import { SET_MODEL, SET_VALUE, setModel, setValue } from './model.actions'
+import reducer from './model'
+
+describe('gui/redux/model', () => {
+  it('should initialize state', () => {
+    // execute
+    const state = reducer(undefined, { type: undefined })
+
+    // asserts
+    expect(state).toBeDefined()
+    expect(state).toEqual({ })
+  })
+
+  describe(`${SET_MODEL} action`, () => {
+    it('should update state with model', () => {
+      // data
+      const model = { foo: {}, bar: {} }
+
+      // execute
+      const state = reducer(undefined, setModel(model))
+
+      // asserts
+      expect(state).toBeDefined()
+      expect(state).toEqual(model)
+    })
+  })
+
+  describe(`${SET_VALUE} action`, () => {
+    it('should update state with "boolean" value', () => {
+      // data
+      const prevState = { foo: { type: 'bool' } }
+
+      // execute
+      const state = reducer(prevState, setValue('foo', true))
+
+      // asserts
+      expect(state).toBeDefined()
+      expect(state.foo).toBeDefined()
+      expect(typeof state.foo.value).toBe('boolean')
+      expect(state.foo.value).toBe(true)
+    })
+
+    it('should update state with "object" value', () => {
+      // data
+      const prevState = { foo: { type: 'object' } }
+
+      // execute
+      const state = reducer(prevState, setValue('foo', '{ bar: \'val\' }'))
+
+      // asserts
+      expect(state).toBeDefined()
+      expect(state.foo).toBeDefined()
+      expect(typeof state.foo.value).toBe('object')
+      expect(state.foo.value).toEqual({ bar: 'val' })
+    })
+
+    it('should update state with "array" value', () => {
+      // data
+      const prevState = { foo: { type: 'array' } }
+
+      // execute
+      const state = reducer(prevState, setValue('foo', '[\'item1\', \'item2\']'))
+
+      // asserts
+      expect(state).toBeDefined()
+      expect(state.foo).toBeDefined()
+      expect(typeof state.foo.value).toBe('object')
+      expect(state.foo.value).toEqual(['item1', 'item2'])
+    })
+
+    it('should update state with "func" value', () => {
+      // data
+      const prevState = { foo: { type: 'func' } }
+      const func = '() => "hello world"'
+
+      // execute
+      const state = reducer(prevState, setValue('foo', func))
+
+      // asserts
+      expect(state).toBeDefined()
+      expect(state.foo).toBeDefined()
+      expect(typeof state.foo.value).toBe('function')
+      expect(state.foo.value.toString()).toEqual(func)
+    })
+
+    it('should update state with "string" value', () => {
+      // data
+      const prevState = { foo: { type: 'string' } }
+
+      // execute
+      const state = reducer(prevState, setValue('foo', 'bar'))
+
+      // asserts
+      expect(state).toBeDefined()
+      expect(state.foo).toBeDefined()
+      expect(typeof state.foo.value).toBe('string')
+      expect(state.foo.value).toEqual('bar')
+    })
+  })
+})
