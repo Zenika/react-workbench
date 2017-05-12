4e6e645e90f369232ec2ca222ce89a1b12f135f5
Benjamin Petetot <bpetetot@gmail.com>
2017-05-12 16:28:55 +0200

    organize docgen api in order to split tests (extract resolvers)

---
+++ src/server/api/models/docgen/resolvers/index.js
@@ -0,0 +1,8 @@
+const componentResolver = require('./componentResolver')
+const indexResolver = require('./indexResolver')
+
+module.exports = Object.assign(
+  {},
+  { componentResolver },
+  { indexResolver }
+)
