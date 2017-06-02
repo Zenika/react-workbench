9f16bdfe3ee6c1b5a898db76a5275c8e33df1695
Benjamin Petetot <bpetetot@gmail.com>
2017-06-02 15:46:22 +0200

    Add some tests for docgen resolver based on reac-docgen tests

--- src/server/api/models/docgen/resolvers/componentResolver.spec.js
+++ src/server/api/models/docgen/resolvers/componentResolver.spec.js
@@ -1,6 +1,52 @@
 /* eslint-env jest */
-require('./componentResolver')
+const recast = require('recast')
+const { parse } = require('../../../../../../misc/recast')
+
+const componentResolver = require('./componentResolver')
 
 describe('server/api/models/docgen/resolvers/componentResolver', () => {
-  it('should test it', () => {})
+  it('should resolve a simple functional component', () => {
+    const parsed = parse(`
+      import React from 'React'
+      const Component = () => {
+        return <div />
+      }
+      export default Component
+    `)
+    const actual = componentResolver(parsed, recast)
+    const expected = parsed.get('body', 1, 'declarations', 0, 'init', 'arguments', 0)
+    expect(actual.length).toBe(1)
+    expect(actual[0].node).toBe(expected.node)
+  })
+
+  it('should resolve a simple component class', () => {
+    const parsed = parse(`
+      import React from 'React'
+      class Component extends React.Component {
+        render() {
+          return <div />
+        }
+      }
+      export default Component
+    `)
+    const actual = componentResolver(parsed, recast)
+    const expected = parsed.get('body', 1, 'declarations', 0, 'init', 'arguments', 0)
+    expect(actual.length).toBe(1)
+    expect(actual[0].node).toBe(expected.node)
+  })
+
+  it.skip('should resolve a simple functional component with HOC', () => {
+    const parsed = parse(`
+      import React from 'React'
+      import { connect } from 'react-redux'
+      const Component = () => {
+        return <div />
+      }
+      export default connect(Component)
+    `)
+    const actual = componentResolver(parsed, recast)
+    const expected = parsed.get('body', 1, 'declarations', 0, 'init', 'arguments', 0)
+    expect(actual.length).toBe(1)
+    expect(actual[0].node).toBe(expected.node)
+  })
 })
