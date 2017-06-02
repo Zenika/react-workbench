7176fdaf131c06d12a54f52b997d64b9bce1967e
Benjamin Petetot <bpetetot@gmail.com>
2017-06-02 14:18:47 +0200

    :white_check_mark: 'fs' api (#74)

--- src/server/api/models/fs/fs.spec.js
+++ src/server/api/models/fs/fs.spec.js
@@ -1,6 +1,63 @@
 /* eslint-env jest */
-require('./service')
+jest.mock('fs', () => ({
+  readdirAsync: jest.fn(async () => undefined),
+  statAsync: jest.fn(async () => Object.setPrototypeOf({}, { isDirectory: () => true })),
+}))
 
-describe('todo', () => {
-  it('todo')
+const fs = require('fs')
+const service = require('./service')
+
+describe('server/api/models/fs', () => {
+  beforeEach(() => {
+    fs.readdirAsync.mockClear()
+    fs.statAsync.mockClear()
+  })
+
+  it('should list folder', async () => {
+    // mocks
+    fs.readdirAsync.mockImplementation(jest.fn(async () => ['baz.txt', 'bat']))
+
+    // calls
+    const result = await service.ls('/foo/bar')
+
+    // asserts
+    expect(result).toBeDefined()
+    expect(result.length).toBe(2)
+  })
+
+  it('should handle empty folder', async () => {
+    // mocks
+    fs.readdirAsync.mockImplementation(jest.fn(async () => []))
+
+    // calls
+    const result = await service.ls('/foo/bar')
+
+    // asserts
+    expect(result).toBeDefined()
+    expect(result.length).toBe(0)
+  })
+
+  it('should give fullname, name, ext, isDirectory and other stats metadata', async () => {
+    // mocks
+    fs.readdirAsync.mockImplementation(jest.fn(async () => ['baz.txt']))
+    fs.statAsync.mockImplementation(
+      jest.fn(async () => Object.setPrototypeOf({
+        foo: 'foo',
+        bar: 'bar',
+      }, { isDirectory: () => false }))
+    )
+
+    // calls
+    const result = await service.ls('/foo/bar')
+
+    // asserts
+    expect(result).toBeDefined()
+    expect(result.length).toBe(1)
+    expect(result[0].fullname).toBe('baz.txt')
+    expect(result[0].name).toBe('baz')
+    expect(result[0].ext).toBe('.txt')
+    expect(result[0].isDirectory).toBe(false)
+    expect(result[0].foo).toBe('foo')
+    expect(result[0].bar).toBe('bar')
+  })
 })
