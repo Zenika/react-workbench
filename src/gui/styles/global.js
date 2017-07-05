2fe74c9372fd18d321721ff8c23448b7b682ade3
Benjamin Petetot <bpetetot@gmail.com>
2017-07-05 11:47:29 +0200

    set global css rules with glamor

---
+++ src/gui/styles/global.js
@@ -0,0 +1,9 @@
+import { css } from 'glamor'
+
+// set global css rules
+css.global('html, body', {
+  padding: 0,
+  margin: 0,
+  fontFamily: 'Arial, Helvetica, sans-serif',
+  fontSize: '13px',
+})
