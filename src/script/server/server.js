53c4a1b5bb1e8828890a3330142b1f5ab709d6fe
Fabien JUIF <fabien.juif@zenika.com>
2017-07-07 17:00:50 +0200

    :construction: functionnal

--- src/script/server/server.js
+++ src/script/server/server.js
@@ -4,11 +4,12 @@ const log = require('loglevel')
 const bundle = require('./bundle')
 const api = require('./api')
 
+// FIXME: move it to root ?
 const PORT = (process.env.PORT || 8080)
 const PUBLIC_FOLDER = path.resolve(__dirname, '..', '..', '..', 'public')
 
 const start = (state) => {
-  const { webpackConfiguration, component } = state
+  const { component } = state
 
   // create a new express server
   const app = express()
@@ -17,7 +18,7 @@ const start = (state) => {
   app.use(express.static(PUBLIC_FOLDER))
 
   // serve webpack bundle
-  bundle.connect(app, webpackConfiguration)
+  bundle.connect(state, app)
 
   // serve api
   api.connect(app, component)
