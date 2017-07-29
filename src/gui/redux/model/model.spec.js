80f6b953992d70d5a3016802dfe34a10a1443f29
Benjamin Petetot <bpetetot@gmail.com>
2017-07-29 18:21:10 +0200

    add redux-factory middleware tests

--- src/gui/redux/model/model.spec.js
+++ src/gui/redux/model/model.spec.js
@@ -1,4 +1,54 @@
 /* eslint-env jest */
+import { createStore, combineReducers } from 'redux'
+import docgen from '../docgen'
+import model from './model'
+
 describe('gui/redux/model', () => {
-  it.skip('should initialize state')
+  let store
+
+  beforeEach(() => {
+    store = createStore(
+      combineReducers({
+        model,
+        docgen,
+      })
+    )
+  })
+
+  it('should transform SET_DOCGEN to SET_MODEL action', () => {
+    // dispatch docgen set action
+    store.dispatch(
+      docgen.set({
+        props: {
+          foo: {
+            type: { name: 'string' },
+            defaultValue: { value: "'foo'" },
+          },
+        },
+      })
+    )
+
+    // check if model state is updated
+    expect(model.isInitialized(store.getState())).toBe(true)
+    expect(model.getKeys(store.getState()).includes('foo')).toBe(true)
+  })
+
+  it('should convert gui values to model values', () => {
+    // dispatch docgen set action
+    store.dispatch(
+      docgen.set({
+        props: {
+          foo: {
+            type: { name: 'string' },
+            defaultValue: { value: "'foo'" },
+          },
+        },
+      })
+    )
+    // dispatch docgen model action
+    store.dispatch(model.update({ name: 'foo', value: 'bar', type: 'string' }))
+
+    // check if model is updated
+    expect(model.get('foo')(store.getState()).value).toBe('bar')
+  })
 })
