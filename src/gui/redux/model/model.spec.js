ad21dc616482f2202d44a4fca35351e6bca212ca
Benjamin Petetot <bpetetot@gmail.com>
2017-08-02 10:18:30 +0200

    :white_check_mark: test model selectors (#112)

--- src/gui/redux/model/model.spec.js
+++ src/gui/redux/model/model.spec.js
@@ -2,53 +2,70 @@
 import { createStore, combineReducers } from 'redux'
 import docgen from '../docgen'
 import model from './model'
+import * as selectors from './model.selectors'
 
 describe('gui/redux/model', () => {
-  let store
+  describe('reducer', () => {
+    let store
 
-  beforeEach(() => {
-    store = createStore(
-      combineReducers({
-        model,
-        docgen,
-      })
-    )
+    beforeEach(() => {
+      store = createStore(combineReducers({ model, docgen }))
+    })
+
+    it('should transform SET_DOCGEN to SET_MODEL action', () => {
+      // dispatch docgen set action
+      store.dispatch(
+        docgen.set({
+          props: {
+            foo: {
+              type: { name: 'string' },
+              defaultValue: { value: "'foo'" },
+            },
+          },
+        })
+      )
+
+      // check if model state is updated
+      expect(store.getState().model).toMatchSnapshot()
+    })
+
+    it('should convert gui values to model values', () => {
+      // dispatch docgen set action
+      store.dispatch(
+        docgen.set({
+          props: {
+            foo: {
+              type: { name: 'string' },
+              defaultValue: { value: "'foo'" },
+            },
+          },
+        })
+      )
+      // dispatch docgen model action
+      store.dispatch(model.update({ name: 'foo', value: 'bar', type: 'string' }))
+
+      // check if model is updated
+      expect(store.getState().model).toMatchSnapshot()
+    })
   })
 
-  it('should transform SET_DOCGEN to SET_MODEL action', () => {
-    // dispatch docgen set action
-    store.dispatch(
-      docgen.set({
-        props: {
-          foo: {
-            type: { name: 'string' },
-            defaultValue: { value: "'foo'" },
-          },
-        },
-      })
-    )
+  describe('selectors', () => {
+    let store
 
-    // check if model state is updated
-    expect(model.isInitialized(store.getState())).toBe(true)
-    expect(model.getKeys(store.getState()).includes('foo')).toBe(true)
-  })
+    beforeEach(() => {
+      store = createStore(combineReducers({ model }))
+    })
 
-  it('should convert gui values to model values', () => {
-    // dispatch docgen set action
-    store.dispatch(
-      docgen.set({
-        props: {
-          foo: {
-            type: { name: 'string' },
-            defaultValue: { value: "'foo'" },
-          },
-        },
-      })
-    )
-    // dispatch docgen model action
-    store.dispatch(model.update({ name: 'foo', value: 'bar', type: 'string' }))
+    it('should get component value in model state', () => {
+      store.dispatch(model.set([{ name: 'foo', value: 'bar', type: 'string' }]))
+      const value = selectors.getComponentValue('foo', 'string')(store.getState())
+      expect(value).toBe('bar')
+    })
 
-    // check if model is updated
-    expect(model.get('foo')(store.getState()).value).toBe('bar')
+    it('should get all component prop in model state', () => {
+      store.dispatch(model.set([{ name: 'foo', value: 'bar', type: 'string' }]))
+      const props = selectors.getComponentProps(store.getState())
+      expect(props).toEqual({ foo: 'bar' })
+    })
   })
 })
