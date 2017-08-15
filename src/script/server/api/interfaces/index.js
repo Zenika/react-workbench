2b436d6a3a2f0aae33d982862a5b01a2b33f613d
Fabien JUIF <fabien.juif@zenika.com>
2017-08-15 10:20:34 +0200

    :sparkles: GET /api/props (#118)

--- src/script/server/api/interfaces/index.js
+++ src/script/server/api/interfaces/index.js
@@ -1,6 +1,8 @@
 const connect = require('./connect')
 const readme = require('./readme')
+const props = require('./props')
 
 module.exports = (app, basePath) => ({
   readme: connect(app, basePath)(readme),
+  props: connect(app, basePath)(props),
 })
