c391d003aa8a205987493c87b9a3e7be90c74061
Fabien JUIF <fabien.juif@zenika.com>
2017-08-16 10:26:45 +0200

    :ok_hand: self-review

--- src/script/server/api/interfaces/preview.js
+++ src/script/server/api/interfaces/preview.js
@@ -3,12 +3,7 @@ const doc = require('../services/doc')
 const post = {
   method: 'post',
   path: 'preview',
-  handler: async (req) => {
-    const markdown = req.body
-    const html = doc.html(markdown)
-
-    return html
-  },
+  handler: async req => doc.html(req.body),
 }
 
 module.exports = {
