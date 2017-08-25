145b1301d4255bf46f1dc1d07ccf660547ce4392
Fabien JUIF <fabien.juif@zenika.com>
2017-08-25 11:55:38 +0200

    :lipstick: state tab

--- src/gui/screens/workbench/state/state.jsx
+++ src/gui/screens/workbench/state/state.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { onlyUpdateForPropTypes } from 'recompose'
+import { merge } from 'glamor'
+import styles from './state.styles'
 
 const State = ({ style, className, state, onChange, setState }) => {
   return (
-    <div style={style} className={className}>
-      <textarea value={state} onChange={onChange} />
+    <div style={style} className={merge(styles.layout, className)}>
       <button onClick={setState}>set</button>
+      <textarea className={styles.textarea} value={state} onChange={onChange} />
     </div>
   )
 }
