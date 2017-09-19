dc962a380a96d2801fbe76a1013c23d3e7c57cd9
Benjamin Petetot <bpetetot@gmail.com>
2017-09-19 16:25:51 +0200

    first styling

--- src/gui/screens/documentation/preview/preview.styles.js
+++ src/gui/screens/documentation/preview/preview.styles.js
@@ -4,6 +4,14 @@ const layout = css({
   height: '100%',
   width: '100%',
   maxHeight: '100%',
+  '& blockquote': {
+    marginLeft: '0',
+    paddingLeft: '1em',
+    borderLeft: '.4em solid #AAA',
+  },
+  '& blockquote p': {
+    lineHeight: '1.4em',
+  },
 })
 
 export default { layout }
