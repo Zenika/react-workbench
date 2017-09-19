6249b5702bdb662bf4f08899ddaf154f3a485879
Benjamin Petetot <bpetetot@gmail.com>
2017-09-19 18:06:52 +0200

    :bug: fix getPropsKeys when props are undefined (#139)

--- src/gui/redux/docgen/docgen.selectors.js
+++ src/gui/redux/docgen/docgen.selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect'
 import { docgen } from '../reducers'
 
-const getProps = state => docgen.get()(state).props
+const getProps = state => docgen.get()(state).props || {}
 export const getPropsKeys = createSelector(getProps, p => Object.keys(p))
 export const getProp = name => createSelector(getProps, p => p[name])
