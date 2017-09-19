9bdae92ba22d7d29eb923e60270860d747e36706
Fabien JUIF <fabien.juif@zenika.com>
2017-09-19 12:18:08 +0200

    :ok_hand: @fabienjuif review

--- src/gui/sagas/documentation/readme.js
+++ src/gui/sagas/documentation/readme.js
@@ -1,10 +1,8 @@
-import { select } from 'redux-saga/effects'
+import { select, call } from 'redux-saga/effects'
 import { readme } from '../../redux/documentation'
 
 export function* saveReadme() {
   const state = yield select(readme.get())
 
-  fetch('/api/readme', { method: 'POST', body: state })
-    .then(() => console.log('saved'))
-    .catch(ex => console.error(ex))
+  yield call(fetch, '/api/readme', { method: 'POST', body: state })
 }
--- src/gui/screens/documentation/actions/actions.jsx
+++ src/gui/screens/documentation/actions/actions.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { onlyUpdateForPropTypes } from 'recompose'
 import SaveButton from '../../../components/button'
 
-const Actions = ({ onSave }) => {
-  return (
-    <div>
-      <SaveButton label="Save" onClick={onSave} />
-    </div>
-  )
-}
+const Actions = ({ onSave }) => <SaveButton label="Save" onClick={onSave} />
 
 Actions.propTypes = {
   onSave: PropTypes.func.isRequired,
 }
 
-export default Actions
+export default onlyUpdateForPropTypes(Actions)
